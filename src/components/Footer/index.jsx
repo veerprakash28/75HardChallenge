1d7cc8fae699186976da4ed1886482ff5aaddba1
Veer Prakash <veer.prakash_cs.aiml19@gla.ac.in>
2023-10-21 00:57:49 +0530

    Removed Rules and added opacity

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="bg-secondary opacity-90 text-white p-2 text-center fixed w-full bottom-0 left-0">
+    <div className="bg-secondary text-white p-2 text-center fixed w-full bottom-0 left-0">
       Made with ❤️ by Veer Prakash
     </div>
   );
